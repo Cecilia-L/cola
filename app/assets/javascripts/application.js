65efd33b4f4db8535f3fb675148fdf36355ee369
norrard148 <norrard@likelion.org>
2018-05-19 12:59:01 +0900

    second

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-
+//= require materialize
 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
